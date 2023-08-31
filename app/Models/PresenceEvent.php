a8171709336a3193e4dbf2ac489cbbcd7191f982
Ismael Guajardo <ish.gdo@gmail.com>
2023-08-31 12:13:37 -0600

    Fixed class ported models class names

--- app/Models/PresenceEvent.php
+++ app/Models/PresenceEvent.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PresenceEvent2 extends Model
+class PresenceEvent extends Model
 {
     use HasFactory;
 
--- app/Models/PusherWebhook.php
+++ app/Models/PusherWebhook.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PusherWebhook2 extends Model
+class PusherWebhook extends Model
 {
     use HasFactory;
 
